33a181b3981a9b25721e7554c6b69cfb42dc9b42
iamtheasad <iamtheasad@gmail.com>
2019-03-06 08:16:05 +0600

    Updates

---
+++ precache-manifest.d88b40e9e5a582cad9cef08fb5531714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de2bf8da835eb85162e",
+    "url": "/face-recognition-app/static/js/runtime~main.6de2bf8d.js"
+  },
+  {
+    "revision": "8941c490e28a4e8ba6aa",
+    "url": "/face-recognition-app/static/js/main.8941c490.chunk.js"
+  },
+  {
+    "revision": "b8657c849f91d6c3edc8",
+    "url": "/face-recognition-app/static/js/1.b8657c84.chunk.js"
+  },
+  {
+    "revision": "8941c490e28a4e8ba6aa",
+    "url": "/face-recognition-app/static/css/main.1e11d24f.chunk.css"
+  },
+  {
+    "revision": "b8657c849f91d6c3edc8",
+    "url": "/face-recognition-app/static/css/1.92aa9987.chunk.css"
+  },
+  {
+    "revision": "bf1b6604deae1822fcc763812aff18a3",
+    "url": "/face-recognition-app/index.html"
+  }
+];
